c4762ecc75d701577330b8e8d268c1fa06de5f95
Manuel <manuel.marino03@gmail.com>
2018-06-10 15:41:15 -0430

    otras ediciones

--- Client/client.py
+++ Client/client.py
@@ -5,7 +5,7 @@
 import socket   #para usar sockets
 import sys      #para entrada de datos
 from form import Form #Formulario 
-
+import pickle #serializacion y codificacion function dumbs() and loads()
  
 # create dgram udp socket
 try:
@@ -23,15 +23,22 @@ apellido = formulario.getLastName()
 ci = formulario.getCI()
 f_n = formulario.getDate()
 lugar_n = formulario.getPlace()
+f_array = formulario.getFotoArray() #bytes de la foto 
+#array de bytes de la foto
+obj = formulario.getSerialize()
 b=True
 while(b) :
-    msg = 'nombre: '+nombre+' apellido: '+apellido+' ci: '+ci+' fecha de Nacimiento: '+f_n+' lugar de nacimiento: '+lugar_n
-     
+    msg = '@nombre: '+nombre+' @apellido: '+apellido+' @ci: '+ci+' @fecha de Nacimiento: '+f_n+' @lugar de nacimiento: '+lugar_n+' @foto: '
+    
     try :
         #Set the whole string
-        s.sendto(msg.encode(), (host, port))
+        s.sendto(obj, (host, port)) #msg.encode()
+        for x in f_array:
+            s.sendto(x, (host, port))
+        #s.sendto('fin'.encode(), (host, port))
+        #s.sendto(foto, (host, port))
          
-        # receive data from client (data, addr)
+        #receive data from client (data, addr)
         d = s.recvfrom(1024)
         reply = d[0]
         addr = d[1]
--- Server/server.py
+++ Server/server.py
@@ -4,7 +4,9 @@
  
 import socket
 import sys
- 
+import pickle # decodificar objeto loads()
+from datetime import datetime, date
+
 HOST = ''   # Symbolic name meaning all available interfaces
 PORT = 7890 # Arbitrary non-privileged port
  
@@ -26,19 +28,54 @@ except socket.error as msg:
      
 print ('Socket bind complete')
  
-#now keep talking with the client
+formato_fecha = "%d-%m-%Y"
 while 1:
     # receive data from client (data, addr)
-    d = s.recvfrom(1024)
+    d = s.recvfrom(224)
     data = d[0]
     addr = d[1]
      
     if not data: 
         break
+
+    try:
+        print(len(data))
+        obj = pickle.loads(data)
+        fecha = obj['fecha_n']
+        fecha_inicial = datetime.strptime(fecha, formato_fecha)
+        hoy = datetime.now()
+        edad = (hoy - fecha_inicial ).days /365
+        edad = int(edad)
+        nombre = obj['nombre']
+        print('edad',edad)
+        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ', obj, ' tam: ')
+    except UnicodeDecodeError as e:
+        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ', data)
+
+    ciclo = True
+    foto_archivo = open('foto_name.png','wb')
+    while ciclo:
+
+        if data != b'fin':
+            d = s.recvfrom(1024)
+            data = d[0]
+            addr = d[1]
+            foto_archivo.write(data)
+        else:
+            foto_archivo.close()
+            break
+                
+
+    #reply = 'OK...' + data.decode()
      
-    reply = 'OK...' + data.decode()
-     
-    s.sendto(reply.encode() , addr)
-    print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ' + data.decode())
-     
+    #s.sendto(reply.encode() , addr)
+    
+    try:
+        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ', data)
+    except UnicodeDecodeError as e:
+        print ('Message[' + addr[0] + ':' + str(addr[1]) + '] - ', data)
+    response = 'Hola '+nombre+ ' de ' + str(edad)+' años.'  
+    s.sendto(response.encode() , addr)
+    break
+
 s.close()
